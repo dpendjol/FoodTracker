5ea7d746856081e180deac18a233f02c63d9105f
Dominique Pendjol <d.pendjol@gmail.com>
2021-03-02 21:07:27 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from .main.routes import main
+
+
+def create_app():
+    app = Flask(__name__)
+    
+    app.register_blueprint(main)    
+    
+    return app
+
+if __name__ == '__main__':
+    create_app()
