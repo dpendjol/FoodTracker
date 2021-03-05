66fd0145381994d09c62c0e807fc2832ae26931c
Dominique Pendjol <d.pendjol@gmail.com>
2021-03-05 22:54:00 +0100

    adjusted the index file

--- index.py
+++ index.py
@@ -2,13 +2,22 @@ from flask import Flask
 from main.routes import main
 
 
-def create_app():
-    app = Flask(__name__)
-    app.secret_key = "justakey"
-    
-    app.register_blueprint(main)    
-    
-    return app
+app = Flask(__name__)
+app.secret_key = "justakey"
+
+app.register_blueprint(main)    
+  
+@app.before_request
+def before_request():
+    """Connect to the database before each request"""
+    db.connect()
+
+
+@app.after_request
+def after_request(response):
+    """Close the database after each request"""
+    db.close()
+    return response
 
 if __name__ == '__main__':
-    create_app()
+    pass
