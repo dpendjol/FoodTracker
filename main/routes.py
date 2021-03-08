09f67baf0efa8bbb7c1757783b050b6e9ba2d53b
Dominique Pendjol <d.pendjol@gmail.com>
2021-03-08 10:22:52 +0100

    made a typo in add route

--- main/routes.py
+++ main/routes.py
@@ -34,7 +34,7 @@ def index():
     return render_template("index.html", logs=log_info)
 
 @main.route('/add', methods=["GET"])
-def add():
+def add()
     foods = Food.select()
     
     return render_template("add.html", foods=foods, food=None)
