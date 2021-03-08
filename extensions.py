b275d1dfd737731154f371b2f95cc9ba097a8a4e
Dominique Pendjol <d.pendjol@gmail.com>
2021-03-08 13:54:43 +0100

    prevent removing db file

--- extensions.py
+++ extensions.py
@@ -1,4 +1,4 @@
 from peewee import SqliteDatabase
 
 # db = SqliteDatabase(':memory:', pragmas={'foreign_keys': 1})
-db = SqliteDatabase('../databases/foodtracker.db', pragmas={'foreign_keys': 1})
+db = SqliteDatabase('foodtracker.db', pragmas={'foreign_keys': 1})
