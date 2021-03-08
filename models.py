581cecc6047b32414240494ab3bab7f6671a8710
Dominique Pendjol <d.pendjol@gmail.com>
2021-03-08 10:19:59 +0100

    corrected some mistakes

--- models.py
+++ models.py
@@ -1,26 +1,29 @@
 from extensions import db
 # from peewee import Model, CharField, IntegerField, DateField, ManyToManyField
-from peewee import *
+from peewee import Model, CharField, IntegerField, DateField, ManyToManyField
+
 
 class Food(Model):
     name = CharField(unique=True)
     protein = IntegerField()
     carbs = IntegerField()
     fats = IntegerField()
-    
+
     @property
     def calories(self):
         return self.protein * 4 + self.carbs * 4 + self.fats * 9
-    
+
     class Meta:
         database = db
-    
+
+
 class Log(Model):
     date = DateField()
     foods = ManyToManyField(Food, backref='logs')
-    
+
     class Meta:
         database = db
+
 
 log_food = Log.foods.get_through_model()
 
