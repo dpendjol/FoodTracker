13e166b253f0597d257e90f1a7bb07a17f61318c
Dominique Pendjol <d.pendjol@gmail.com>
2021-03-11 19:27:12 +0100

    the tests are working

--- index.py
+++ index.py
@@ -8,23 +8,23 @@ def create_app():
     app.secret_key = "justakey"
 
     app.register_blueprint(main)    
+    
+    @app.before_request
+    def before_request():
+        """Connect to the database before each request"""
+        db.connect()
+
+
+    @app.after_request
+    def after_request(response):
+        """Close the database after each request"""
+        db.close()
+        return response
+
+    @app.context_processor
+    def inject_now():
+        return {'now': datetime.utcnow()}
     return app
-  
-@app.before_request
-def before_request():
-    """Connect to the database before each request"""
-    db.connect()
-
-
-@app.after_request
-def after_request(response):
-    """Close the database after each request"""
-    db.close()
-    return response
-
-@app.context_processor
-def inject_now():
-    return {'now': datetime.utcnow()}
 
 if __name__ == '__main__':
     pass
